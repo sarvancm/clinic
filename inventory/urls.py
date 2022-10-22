f2eff1410264236795bdeaf11231ab215f79e953
sarvancm <108647664+sarvancm@users.noreply.github.com>
2022-10-22 14:21:00 +0530

    update medicine

--- inventory/urls.py
+++ inventory/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
  
     path('', views.add_medicine, name='inventory_add_medicine'),
     path('search_medicine/', views.search_medicine, name='inventory_search_medicine'),
+    path('update_medicine/', views.update_medicine, name='inventory_update_medicine'),
   
     # path('register/', views.register, name='register'),  
 
--- inventory/views.py
+++ inventory/views.py
@@ -14,7 +14,7 @@ def add_medicine(request):
         form = MedicineForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, 'expencess added successfully')
+            messages.success(request, 'medicine added successfully')
             form = MedicineForm()
             return render(request,'inventory/add_medicine.html', {'form': form,'medicine':incrementid()})
         else:
@@ -52,6 +52,46 @@ def search_medicine(request):
 
         return render(request,'inventory/search_medicine.html', {})
 
+
+# update_medicine
+def update_medicine(request):
+
+    if request.method == 'POST':
+        id = request.POST.get('object_id')
+        name_id=request.POST.get('search')
+        start_date=request.POST.get('start_date')
+        end_date=request.POST.get('end_date')
+        medi=Medicine.objects.filter(Q(medicine_name=name_id)|Q(medicine_id=name_id))
+        medi_id=[i.id for i in medi] 
+        try:
+            a = datetime.datetime.strptime( start_date, '%Y-%m-%d').date()
+            b = datetime.datetime.strptime( end_date, '%Y-%m-%d').date()
+            medicines=Medicine.objects.filter(created_at__date__gte=a,created_at__date__lte=b)
+        except:
+            a=start_date
+            b=end_date
+            medicines=[]
+        if medi_id and medicines:
+            medicines=[i for i in medicines if i.id in medi_id]
+        elif medi:
+            medicines=medi
+        else:
+            medicines = medicines
+        object=Medicine.objects.get(id=id)
+        form = MedicineForm(request.POST,instance=object)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'medicine updated successfully')
+            
+            return render(request,'inventory/search_medicine.html', {'medicines': medicines,'name_id':name_id,'start_date':start_date,'end_date':end_date})
+
+        else:
+            print(form.errors)
+            return render(request,'inventory/search_medicine.html', {'object':id,'err':True,'form':form,'medicines': medicines,'name_id':name_id,'start_date':start_date,'end_date':end_date})
+
+    else:
+        
+        return redirect('inventory_search_medicine')
 
 
 
